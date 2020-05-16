f67e83e03eaebe05e090dae01154acb32b7974b7
JialongWen <631488786@qq.com>
2020-05-16 11:55:03 +0800

    init project

---
+++ src/main/java/com/wjl/eurc/EurcApplication.java
@@ -0,0 +1,13 @@
+package com.wjl.eurc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurcApplication.class, args);
+    }
+
+}
